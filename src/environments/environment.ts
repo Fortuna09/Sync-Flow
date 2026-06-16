d2022056740a05f15ec65d605594bec9c0bf3557
Rafael Fortuna <132229277+Fortuna09@users.noreply.github.com>
2026-06-16 15:49:53 -0300

    Update Supabase credentials in environment file kakakakakaka

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 // Produção - placeholders substituídos pelo CI/CD
 export const environment = {
   production: true,
-  supabaseUrl: 'SUPABASE_URL_PLACEHOLDER',
-  supabaseKey: 'SUPABASE_KEY_PLACEHOLDER'
+  supabaseUrl: 'https://fjgcxefcvdkwnsnttwqo.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZqZ2N4ZWZjdmRrd25zbnR0d3FvIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4MTYxODgyMSwiZXhwIjoyMDk3MTk0ODIxfQ.oKGwZSLvo2wSYZVLc_LdA3GuXTNYgddXDYG9nykI-io'
 };
