f6b7b3c26fb381477c61ed3fdea05f631f23a190
Rafael Fortuna <132229277+Fortuna09@users.noreply.github.com>
2026-06-16 15:36:44 -0300

    refactor: troca de banco (supabase)

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 // Desenvolvimento - arquivo gitignored
 export const environment = {
   production: false,
-  supabaseUrl: 'https://tfglmkixbvlnrjjsrdwa.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRmZ2xta2l4YnZsbnJqanNyZHdhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU1MDc4NTAsImV4cCI6MjA4MTA4Mzg1MH0.eF4y2QPQ1fce2mdhZ9mq_nV8Z9RkAixirTZu8FHSovY'
+  supabaseUrl: 'https://fjgcxefcvdkwnsnttwqo.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZqZ2N4ZWZjdmRrd25zbnR0d3FvIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4MTYxODgyMSwiZXhwIjoyMDk3MTk0ODIxfQ.oKGwZSLvo2wSYZVLc_LdA3GuXTNYgddXDYG9nykI-io'
 };
